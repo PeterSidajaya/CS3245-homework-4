1a813fb4b4137f64d8f59ba23ec99ed3fd82c1c9
Peter <Peter@MacBook-Pro.local>
2021-04-17 15:29:59 +0800

    change indexing to parse a csv file and include positional list

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from spimi import count_word
+from index_helper import index_text
 
 import re
 import nltk
@@ -9,69 +9,79 @@ import os
 import pickle
 import math
 import string
+import csv
 
 def usage():
     print("usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file")
     
 
 # main function
-def build_index(in_dir, out_dict, out_postings):
+def build_index(doc_id, out_dict, out_postings):
     """
-    build index from documents stored in the input directory,
+    build index from the csv file,
     then output the dictionary file and postings file
     """
     stemmer = nltk.stem.PorterStemmer()
-    filename_list = []
-
-    if (in_dir[-1] != '/'):
-        in_dir += '/'
-    for filename in os.listdir(in_dir):
-        filename_list.append(int(filename))
-    filename_list.sort()
-
+    sample_limit = 30                      # this is the limit of the entry we are taking
     dictionary = {}
-    dictionary['LENGTH'] = {}
+    dictionary['LENGTH'] = {}               # this is where we'll store the length of each docs
 
-    for filename in filename_list:
-        document = open(in_dir + str(filename), 'r', encoding="utf8")
-        document_text = document.read().splitlines()
-        document.close()
-        document_list = [nltk.tokenize.word_tokenize(text) for text in document_text]
-        flattened_list = [text for ls in document_list for text in ls]
-        filtered_list = [text for text in flattened_list if text not in string.punctuation] # remove punctuation as discussed in the forum
-        token_list = list(map(lambda x: stemmer.stem(x).lower(), filtered_list))
-        
-        count_dict = count_word(token_list)
-        length = 0
-    
-        # calculate and precompute doc_freq and posting_lists
-        for term, tf in count_dict.items():
-            if term not in dictionary:
-                dictionary[term] = (1, [(int(filename), tf)])
-            else:
-                df = dictionary[term][0] + 1
-                posting_list = dictionary[term][1] + [(int(filename), tf)]
-                dictionary[term] = (df, posting_list)
-            length += (1 + math.log(tf, 10)) ** 2
-        
-        # calculate document length for document normalization in search
-        dictionary['LENGTH'][int(filename)] = math.sqrt(length)
+    # this part opens the csv
+    csv.field_size_limit(sys.maxsize)
+    with open(doc_id, newline='') as f:
+        reader = csv.reader(f)
+        i = 0
 
+        for row in reader:
+            i += 1
+            if i == 1:                  # skip first row
+                continue
+            if i == sample_limit + 2:
+                break
+
+            doc_id = row[0]
+            title = row[1]
+            content = row[2]
+            date_posted = row[3]
+            court = row[4]
+
+            word_list = nltk.tokenize.word_tokenize(content)
+            filtered_list = [text for text in word_list if text not in string.punctuation]
+            token_list = list(map(lambda x: stemmer.stem(x).lower(), filtered_list))
+
+            content_dict = index_text(token_list)
+            length = 0
+
+            # calculate and precompute df and length
+            for term, tup in content_dict.items():
+                tf, position_list = tup
+                if term not in dictionary:
+                    dictionary[term] = (1, [(int(doc_id), tf, position_list),])      # first entry is df
+                else:
+                    df = dictionary[term][0] + 1
+                    posting_list = dictionary[term][1] + [(int(doc_id), tf, position_list),]
+                    dictionary[term] = (df, posting_list)
+                length += (1 + math.log(tf, 10)) ** 2       # document length is calculated from tf
+            
+            # calculate document length for document normalization in search
+            dictionary['LENGTH'][int(doc_id)] = math.sqrt(length)
+
+    # this part here deals with pickles
     posting_file = open(out_postings, 'wb')
     dictionary_file = open(out_dict, 'wb')
-    posting_file.truncate(0)
+    posting_file.truncate(0)                    # delete existing contents
     dictionary_file.truncate(0)
 
     # store posting_lists and dictionary to files
     for term, value in dictionary.items():
-        if term == 'LENGTH':
+        if term == 'LENGTH':                    # LENGTH is not a term
             continue
         df, posting_list = value
-        pointer = posting_file.tell()
-        pickle.dump(posting_list, posting_file)
-        dictionary[term] = (df, pointer)
+        pointer = posting_file.tell()           # find our current position in the posting file
+        pickle.dump(posting_list, posting_file) # dump the posting list into the posting file
+        dictionary[term] = (df, pointer)        # keep track of the pointer
         
-    pickle.dump(dictionary, dictionary_file)
+    pickle.dump(dictionary, dictionary_file)    # dumpt the dictionary into the dictionary file
     posting_file.close()
     dictionary_file.close()
 
---
+++ index_helper.py
@@ -0,0 +1,39 @@
+import pickle
+
+def index_text(token_list):
+    """convert a stream of tokens into a dictionary of term, (frequency, position list) pairs
+
+    Args:
+        token_list (list): list of tokens
+
+    Returns:
+        dict: dictionary of term, (frequency, position list) pairs
+    """
+    dict = {}
+    index = 0
+    for term in token_list:
+        if term not in dict:
+            dict[term] = (1, [index,])
+        else:
+            count, position_list = dict[term]
+            count += 1
+            position_list.append(index)
+            dict[term] = (count, position_list)
+        index += 1
+    return dict
+
+def get_list(term, dictionary, posting_file):
+    """Returns you the posting list
+
+    Args:
+        term (str): the term you want to find the posting list of
+        posting_file (python file object): use this format -> open(filename, 'rb')
+
+    Returns:
+        list: [(doc Id, term frequency, position list), ...]
+    """
+
+    pointer = dictionary[term][1]
+    posting_file.seek(pointer)
+    posting_list = pickle.load(posting_file)
+    return posting_list
