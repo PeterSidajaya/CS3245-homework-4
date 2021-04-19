ea1a7beab972a28856e045baf9f4800e18fae5ca
Anthony Halim <anthony.halim@live.com>
2021-04-19 12:36:33 +0800

    Handle implicit OR for parsing

--- query_util.py
+++ query_util.py
@@ -2,11 +2,15 @@ from enum import Enum
 from constants import *
 
 import re
+import math
 
 class QueryType(Enum):
     FREE_TEXT = 0
     PHRASAL = 1
 
+class BooleanOp(Enum):
+    AND = 0
+    OR = 1
 
 def categorise_query(query: str):
     """
@@ -18,23 +22,60 @@ def categorise_query(query: str):
     e.g. Input: "little puppy" AND chihuahua 
          Output: [<little puppy, QueryType.PHRASAL>, <chihuahua, QueryType.FREE_TEXT>]
     """
-    # Try to split by boolean op
-    exp_split = query.split("AND")
-
-    # Check for each clause its type and append it
-    query_clauses = []
-    for exp in exp_split:
-        phrasal_search = re.search(PHRASAL_REGEX, exp)
-        if (phrasal_search == None):
-            query_clauses.append((exp.strip(), QueryType.FREE_TEXT))
-        else:
-            query_clauses.append((phrasal_search.group(1).strip(), QueryType.PHRASAL))
+    if (len(query) < 1):
+      return
     
+    curr_str_idx = 0
+    query_clauses = []
+    is_last_keyword_quote = False
+
+    # i = 0
+    while curr_str_idx != -1 and curr_str_idx < len(query):
+        spliced_query = query[curr_str_idx:]
+        clause = ""
+        closest_keyword_pos = 0
+        closest_keyword = ""
+
+        print("Spliced query: {}".format(spliced_query))
+        if (is_last_keyword_quote):
+            # Find the next double quote
+            closest_keyword_pos = spliced_query.find(DOUBLE_QUOTE_KEYWORD)
+            closest_keyword = DOUBLE_QUOTE_KEYWORD
+
+            is_last_keyword_quote = False
+            print("Finding the next DQUOTE: {}".format(closest_keyword_pos))
+        
+        else:
+            # Finding closest query keyword
+            keywords_pos = list(map(lambda keyword: spliced_query.find(keyword), QUERY_KEYWORDS))
+            closest_keyword_idx = find_closest_idx(keywords_pos)
+            closest_keyword_pos = keywords_pos[closest_keyword_idx]
+            closest_keyword = QUERY_KEYWORDS[closest_keyword_idx]
+
+            is_last_keyword_quote = closest_keyword == DOUBLE_QUOTE_KEYWORD
+            print("Finding the next KEYWORD: {}".format(closest_keyword_pos))
+        
+        # If there is no more keyword, we splice until the end of the string
+        next_idx = len(spliced_query) if closest_keyword_pos == -1 else closest_keyword_pos
+        clause = spliced_query[:next_idx].strip()
+        print("Curr idx: {}, next_idx: {}".format(curr_str_idx, next_idx))
+        if (len(clause) > 0):
+          print("Clause: {}".format(clause))
+
+        # Update position
+        curr_str_idx = closest_keyword_pos if closest_keyword_pos == -1 else curr_str_idx + next_idx + len(closest_keyword)
+        print("New curr Idx: {}".format(curr_str_idx))
+        print()
+
     return query_clauses
 
+############ HELPERS ############
 
-# TODO: What is this?
-def stem_clause(clause: (str, QueryType), stemmer):
-    """
-    """
-    return (stemmer.stem(clause[0]).lower(), clause[1]) 
+def find_closest_idx(keywords_pos):
+    smallest_idx = math.inf
+    for idx, pos in enumerate(keywords_pos):
+        if pos > -1 and pos < smallest_idx:
+            smallest_idx = idx
+
+    smallest_idx = -1 if math.isinf(smallest_idx) else smallest_idx
+    return smallest_idx
