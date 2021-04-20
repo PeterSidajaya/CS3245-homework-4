a44a280980da435165ed1d387c05e6da8fc1d735
Anthony Halim <50617144+anthony-halim@users.noreply.github.com>
2021-04-20 08:54:15 +0800

    Update query categorisation, new query logic (#9)
    * Handle implicit OR for parsing
    
    * Classify query clause type and boolean operator
    
    * Fix logic for query categorising
    
    * New query logic (#10)
    
    * First attempt at new query logic
    
    * Finish implementation of query logic
    
    * Remove unnecessary code from free_text_search
    
    * Remove unnecessary code from free_text_search
    
    * fix bugs
    
    * Fix typo
    
    * Fix free_text_search to return list instead of string
    
    Co-authored-by: victorvic54 <sacchirro@yahoo.com>

--- query.py
+++ query.py
@@ -1,157 +1,49 @@
+from nltk.stem import WordNetLemmatizer
 from collections import Counter
+
+from free_text_query import free_text_search
 from phrasal_query import get_phrasal_query_doc_id
-from query_util import categorise_query, QueryType
-import nltk
-import pickle
-import math
-
-# this function will be used to run if there is phrasal query
-def intersect_document_ids(doc_list):
-    if (len(doc_list) == 1):
-        return doc_list[0]
-
-    # sort by lower domain first
-    doc_list.sort(key=len)
-    return set(doc_list[0]).intersection(*doc_list[1:])
-
+from query_expansion import expand_clause
+from query_util import *
+from constants import *
 
 def process_query(query_string, dictionary, posting_file):
+    lemmatzr = WordNetLemmatizer()
+
     # Categorise query
     query_clauses = categorise_query(query_string)
-    phrasal_results = []
-
-    # We only do search on phrasal queries first
-    contains_phrasal_query = False
-
-    for query_clause in query_clauses:
-        if query_clause[1] == QueryType.PHRASAL:
-            phrasal_query_docs = get_phrasal_query_doc_id(query_clause[0], dictionary, posting_file)
-            phrasal_results.append(phrasal_query_docs)
-            contains_phrasal_query = True
-
-    # From the list of phrasal results, we do free text search
-    query_list = " ".join([word for word, word_type in query_clauses]).split(" ")
-
-    if contains_phrasal_query:
-        # Combine phrasal results
-        phrasal_result_doc_id = intersect_document_ids(phrasal_results)
-        final_result = free_text_search(query_list, dictionary, posting_file, phrasal_result_doc_id)
-    else:
-        final_result = free_text_search(query_list, dictionary, posting_file, None)
-
-    return final_result
-
-
-def free_text_search(query_list, dictionary, posting_file, accepted_doc_id):
-    """rank the list of document based on the query given
-
-    Args:
-        query_list (list): the list of query string to be ranked against
-        dictionary (dictionary): dictionary of the posting lists
-        posting_file (str): address to the posting file list
-        accepted_doc_id (set): set of valid doc_id from phrasal queries in the given query text
-
-    Returns:
-        str: search rank result
-    """
-    # all tokenization should consistent with the one from index.py
-    stemmer = nltk.stem.PorterStemmer()
-    token_list = list(map(lambda x: stemmer.stem(x).lower(), query_list))
-    query_counter = Counter(token_list)
-    query_keys = list(query_counter.keys())
-
-    query_length = 0
-    query_term_vector = []
-
-    # To get a faster quering, we precomute the value for tf_idf query vector
-    # Next time, we only need to do dot product with each of the given document 
-    for term in query_keys:
-        tf_idf_score = 0
-
-        # dictionary is in the form of:
-        # dict = {
-        #   "term1": (df1, pointer_to_posting_file),
-        #   "term2": (df2, pointer_to_posting_file),
-        #   ...
-        # }
-        #
-        # posting_list is in the form of:
-        # [(docID_1, tf1), (docID2, tf2), ...]
-        if (term in dictionary):
-            term_info = dictionary[term]
-            term_df = term_info[0]
-            term_pointer = term_info[1]
-            no_of_document = len(dictionary["LENGTH"])
-
-            posting_file.seek(term_pointer)
-            posting_list = pickle.load(posting_file)
-
-            tf_idf_score = (1 + math.log(query_counter[term], 10)) * math.log(no_of_document / term_df)
-            query_length += (tf_idf_score ** 2)
-        
-        query_term_vector.append(tf_idf_score)
-
-    normalize_denominator = math.sqrt(query_length)
-    if (normalize_denominator != 0):
-        # final precompute query vector
-        query_term_vector = normalize_list(query_term_vector, normalize_denominator)
-
-
-    # dictionary["LENGTH"] is the normalize denominator for a particular document_id which precomputed in index stage
-    ranking_list = []
-    potential_document_id = set()
-    document_term_dict = {}
-
-    # initialize the dictionary
-    for term in query_keys:
-        document_term_dict[term] = {}
-
-    # calculate tf_idf_score for each term (if it exists in the dictionary)
-    for term in query_keys:
-        tf_idf_score = 0
-
-        if (term in dictionary):
-            term_info = dictionary[term]
-            term_pointer = term_info[1]
-            
-            posting_file.seek(term_pointer)
-            posting_list = pickle.load(posting_file)
-            
-            for (doc_id, term_freq, _) in posting_list:
-                tf_idf_score = 1 + math.log(term_freq, 10)  # tf
-                document_term_dict[term][doc_id] = tf_idf_score / dictionary["LENGTH"][doc_id]  # normalize score
-                potential_document_id.add(doc_id)
     
-    # sort the list in case two or more document_id score the same
-    potential_document_id = sorted(list(potential_document_id))
+    # Lemmatise
+    lemmatized_query_clauses = list(map(lambda clause: (lemmatzr.lemmatize(clause[0]).lower(), clause[1], clause[2]), query_clauses))
 
-    # calculate and rank document_id based on cosine score
-    for doc_id in potential_document_id:
-        document_term_vector = []
-        score = []
+    last_op_type = BooleanOp.OR
+    results = []
+    
+    for query_clause in lemmatized_query_clauses:
+        clause_word, clause_type, op_type = query_clause
+        curr_result = []
 
-        # calculate cosine score
-        for i in range(len(query_keys)):
-            term = query_keys[i]
+        # Get result for the current clause
+        if clause_type == QueryType.PHRASAL:
+            curr_result = get_phrasal_query_doc_id(clause_word, dictionary, posting_file)
+        elif clause_type == QueryType.FREE_TEXT:
+            # If we want to use clause expansion, use this
+            # clause_word = expand_clause(query_clause)
+            
+            free_text_list = clause_word.split(" ")
+            curr_result = free_text_search(free_text_list, dictionary, posting_file, None, do_ranking=False)
 
-            if (term not in document_term_dict or doc_id not in document_term_dict[term]):
-                score.append(0)
-            else:
-                score.append(document_term_dict[term][doc_id] * query_term_vector[i])
+        # Combine with the existing results
+        if last_op_type == BooleanOp.AND:
+            results = intersect_document_ids(results, curr_result)
+        elif last_op_type == BooleanOp.OR:
+            results = union_document_ids(results, curr_result)
 
-        # final cosine score for ranking
-        score = sum(score)
+        # Update for next clause
+        last_op_type = op_type
 
-        ranking_list.append((score, doc_id))
-        ranking_list.sort(key=lambda x: x[0], reverse=True)
+    # Score and rank
+    query_list = " ".join([clause_word for clause, clause_type, op_type in query_clauses]).split(" ")
+    final_result = free_text_search(query_list, dictionary, posting_file, results, do_ranking=True)
 
-    if (accepted_doc_id == None):
-        tf_idf_doc_list = [y for x, y in ranking_list]
-    else:
-        tf_idf_doc_list = [y for x, y in ranking_list if y in accepted_doc_id]
-        
-    return " ".join(str(doc_id) for doc_id in tf_idf_doc_list)
-
-
-def normalize_list(lst, denominator):
-    return list(map(lambda x: x/denominator, lst))
+    return " ".join(str(doc_id) for doc_id in final_result)
--- query_util.py
+++ query_util.py
@@ -2,39 +2,104 @@ from enum import Enum
 from constants import *
 
 import re
+import math
 
 class QueryType(Enum):
     FREE_TEXT = 0
     PHRASAL = 1
 
+class BooleanOp(Enum):
+    AND = 0
+    OR = 1
 
 def categorise_query(query: str):
     """
     Output a list of clauses, where each element is a tuple
-    of the clause and its type.
+    of the clause, its type, and the boolean operator to connect with the next element.
     
-    Multiple clause implies that each clause is connected via AND operator.
+    Each clause will be tagged with the clause type and its boolean operator.
+    Boolean operator is used to connect to the next clause.
+
+    This method reads the query from left to right; stopping at every keywords ('"', AND)
+    and determine that the substring in between as keywords as clauses.
 
     e.g. Input: "little puppy" AND chihuahua 
-         Output: [<little puppy, QueryType.PHRASAL>, <chihuahua, QueryType.FREE_TEXT>]
+         Output: [('little puppy', <QueryType.PHRASAL>, <BooleanOp.AND>), ('chihuahua', <QueryType.FREE_TEXT>, <BooleanOp.OR>)]
     """
-    # Try to split by boolean op
-    exp_split = query.split("AND")
-
-    # Check for each clause its type and append it
-    query_clauses = []
-    for exp in exp_split:
-        phrasal_search = re.search(PHRASAL_REGEX, exp)
-        if (phrasal_search == None):
-            query_clauses.append((exp.strip(), QueryType.FREE_TEXT))
-        else:
-            query_clauses.append((phrasal_search.group(1).strip(), QueryType.PHRASAL))
+    if (len(query) < 1):
+      return
     
+    curr_str_idx = 0
+    query_clauses = []
+    is_last_keyword_quote = False
+
+    while curr_str_idx != -1 and curr_str_idx < len(query):
+        spliced_query = query[curr_str_idx:]
+        clause = ""
+        clause_type = None
+        closest_keyword_pos = 0
+        closest_keyword = ""
+
+        if (is_last_keyword_quote):
+            # Find the next double quote
+            closest_keyword_pos = spliced_query.find(DOUBLE_QUOTE_KEYWORD)
+            closest_keyword = DOUBLE_QUOTE_KEYWORD
+
+            is_last_keyword_quote = False
+            clause_type = QueryType.PHRASAL
+        
+        else:
+            # Finding closest query keyword
+            keywords_pos = list(map(lambda keyword: spliced_query.find(keyword), QUERY_KEYWORDS))
+            closest_keyword_idx = find_closest_idx(keywords_pos)
+            closest_keyword_pos = keywords_pos[closest_keyword_idx]
+            closest_keyword = QUERY_KEYWORDS[closest_keyword_idx]
+
+            is_last_keyword_quote = closest_keyword == DOUBLE_QUOTE_KEYWORD
+            clause_type = QueryType.FREE_TEXT
+        
+        # If there is no more keyword, we splice until the end of the string
+        next_idx = len(spliced_query) if closest_keyword_pos == -1 else closest_keyword_pos
+        clause = spliced_query[:next_idx].strip()
+
+        if (len(clause) > 0):
+            op_type = BooleanOp.AND if closest_keyword == AND_KEYWORD else BooleanOp.OR
+            query_clauses.append((clause, clause_type, BooleanOp.OR))
+
+        # When we encounter AND, the previous clause is connected by AND
+        if (closest_keyword == AND_KEYWORD and len(query_clauses) > 0):
+            query_clauses[-1] = (query_clauses[-1][0], query_clauses[-1][1], BooleanOp.AND)
+
+        # Update position
+        curr_str_idx = closest_keyword_pos if closest_keyword_pos == -1 else curr_str_idx + next_idx + len(closest_keyword)
+
     return query_clauses
 
+def intersect_document_ids(doc_list1, doc_list2):
+    doc_list = [doc_list1, doc_list2]
 
-# TODO: What is this?
-def stem_clause(clause: (str, QueryType), stemmer):
+    # sort by lower domain first
+    doc_list.sort(key=len)
+    return list(set(doc_list[0]).intersection(*doc_list[1:]))
+
+def union_document_ids(doc_list1, doc_list2):
+    doc_list = [doc_list1, doc_list2]
+
+    return list(set(doc_list[0]).union(*doc_list[1:]))
+
+############ HELPERS ############
+
+def find_closest_idx(keywords_pos):
     """
+    Given a list of integer, returns the smallest index that is above -1.
     """
-    return (stemmer.stem(clause[0]).lower(), clause[1]) 
+    smallest_idx = math.inf
+    smallest_pos = math.inf
+
+    for idx, pos in enumerate(keywords_pos):
+        if pos > -1 and pos < smallest_pos:
+            smallest_idx = idx
+            smallest_pos = pos
+
+    smallest_idx = -1 if math.isinf(smallest_idx) else smallest_idx
+    return smallest_idx
